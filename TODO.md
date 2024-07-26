dfbe32d90f8203a60b589c7dfaa537493bbed36f
jhonuel <58693667+jhonuel@users.noreply.github.com>
2024-07-26 11:01:52 -0400

    borrado

--- TODO.md
+++
@@ -1,9 +0,0 @@
-
-Cart :
-- [] Cart_iten images
-- [] agregar varios productos con descripciones y imagenes elegantes
-- [] imagenes de productos
-- [] view de los formularios
-  - add producto
-  - become vendor
-  - 
--- ecommerce/filters.py
+++
@@ -1,9 +0,0 @@
-from django_filters.rest_framework import FilterSet
-
-from ecommerce.models import Product
-
-
-class ProductFilter(FilterSet):
-    class Meta:
-        model = Product
-        fields = {"category": ["exact"], "seller": ["exact"], "price": ["gt", "lt"]}
